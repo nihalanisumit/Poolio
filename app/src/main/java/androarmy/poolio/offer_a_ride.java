4b0de316b8e70e10d6c24f97e86b506cc0a6402a
Poolio <poolio.travel@gmail.com>
2016-06-16 02:06:08 +0530

    Changes in Offer a ride. PHP script and database also updated.

--- app/src/main/java/androarmy/poolio/offer_a_ride.java
+++ app/src/main/java/androarmy/poolio/offer_a_ride.java
@@ -1,12 +1,15 @@
 package androarmy.poolio;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,21 +23,25 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
-import android.app.TimePickerDialog;
+
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 
 public class offer_a_ride extends Fragment {
+
+    //public final String REGISTER_URL="http://192.168.1.101/poolio/register.php"; //Siddharth's pc
+    public final String OFFER_URL="http://192.168.1.10:8080/poolio/offer.php";// Sumit's pc
+
     String[] locations ={"SRM Arch Gate","Abode Valley","Estancia","Backgate","Potheri Station","Guduvancheri"};//need to make it dynamic
     List<String> vehicleType = new ArrayList<String>(); //No need for dynamic i suppose
     public static Spinner spinner;
     AutoCompleteTextView actv,actv2;
     String source, destination, type,mobile,date,time,vname,vnumber;
     int availableSeats, amount=0;
-    public boolean chargeable=true; //false for free ride
+    public int chargeable=1; //false for free ride
     SharedPreferences pref;
     EditText sourceET,destinationET, dateET, timeET,vnameET,vnumberET,availableET,amountET;
     static RadioGroup chargeableRG;
@@ -93,12 +100,12 @@ public class offer_a_ride extends Fragment {
                     chargeLayout.setVisibility(View.GONE);
                     Toast.makeText(getContext(),"Thanks for being so Generous",Toast.LENGTH_SHORT).show();
                     amount=0;
-                    chargeable=false;
+                    chargeable=0;
                 }
                 else
                 {
                     chargeLayout.setVisibility(View.VISIBLE);
-                    chargeable=true;
+                    chargeable=1;
                 }
             }
         });
@@ -161,7 +168,7 @@ public class offer_a_ride extends Fragment {
         vname=vnameET.getText().toString();
         vnumber=vnumberET.getText().toString();
         availableSeats=Integer.parseInt(availableET.getText().toString());
-        //boolean chargeable will be set automatically depending on radio button selected
+        //int chargeable will be set automatically depending on radio button selected
         if(chargeableRG.getCheckedRadioButtonId()==R.id.radio_charge)
         {
             amount=Integer.parseInt(amountET.getText().toString());
@@ -170,9 +177,63 @@ public class offer_a_ride extends Fragment {
         //Toast.makeText(getContext(),"Mobile :"+mobile+"\n Source:"+source+"  Destination:"+destination+"\ntype:"+type
               //  +"\nSeats:"+availableSeats+"\n Amount:"+amount,Toast.LENGTH_LONG).show();
 
+        offer(mobile,source,destination,type,date,time,vname,vnumber,availableSeats,chargeable,amount);
+    }
+
+    //11 parameters
+    public void offer(String mobile,String source,String destination,String type,String date,String time, String vname, String vnumber, final int availableSeats, int chargeable,int amount){
+        class OfferTheRide extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+            RegisterUserClass ruc=new RegisterUserClass();
 
 
+            protected void onPreExecute() {
 
+                super.onPreExecute();
+                loading = ProgressDialog.show(getContext(), "Saving Your Details","Thanks for offering ride", true, true);
+            }
+            protected void onPostExecute(String s){
+                super.onPostExecute(s);
+                loading.dismiss();
+                if("".equals(s))
+                {
+                    s="Server error, Please try again after some time!";
+                }
+                else if("successfully registered".equalsIgnoreCase(s)){
+                    Intent myIntent = new Intent(getContext(), Confirmation.class);//Need to make an activity saying your ride has been offered succesfully and will be displayed soon
+                    startActivity(myIntent);
+                    //getActivity().overridePendingTransition(R.anim.next_slide_in, R.anim.next_slide_out);
+                    Toast.makeText(getContext(),"Ride offered",Toast.LENGTH_SHORT).show();
+                }
+
+                Toast.makeText(getContext(),s,Toast.LENGTH_LONG).show();
+
+
+            }
+
+
+            @Override
+            protected String doInBackground(String... params) {
+                HashMap<String, String> data = new HashMap<String,String>();
+                data.put("mobile",params[0]);
+                data.put("source",params[1]);
+                data.put("destination",params[2]);
+                data.put("type",params[3]);
+                data.put("date",params[4]);
+                data.put("time",params[5]);
+                data.put("vname",params[6]);
+                data.put("vnumber",params[7]);
+                data.put("availableSeats",params[8]);
+                data.put("chargeable",params[9]);
+                data.put("amount",params[10]);
+                String result = ruc.sendPostRequest(OFFER_URL,data);
+                //Log.i("@doinBackground:", result);
+                return  result;
+
+            }
+        }
+        OfferTheRide otr = new OfferTheRide();
+        otr.execute(mobile,source,destination,type,date,time,vname,vnumber,Integer.toString(availableSeats),Integer.toString(chargeable),Integer.toString(amount));
     }
 
 
