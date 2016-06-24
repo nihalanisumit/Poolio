3180a3300433c1b99c206c3be1e70ae76cc57bf4
abhijeet1997 <abhijeetroxx117@gmail.com>
2016-06-24 13:12:45 +0530

    splash screen image updated

--- app/src/main/java/androarmy/poolio/SplashScreen.java
+++ app/src/main/java/androarmy/poolio/SplashScreen.java
@@ -1,20 +1,50 @@
 package androarmy.poolio;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.CountDownTimer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class SplashScreen extends AppCompatActivity {
+
+public class SplashScreen extends Activity {
     MyCountdownTimer myCountdownTimer;
+    Animation fadeIn;
+    private ImageView logo;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        StartAnimations();
         myCountdownTimer = new MyCountdownTimer(3000, 1000);
         myCountdownTimer.start();
 
+        logo = (ImageView)findViewById(R.id.logo);
+
+        fadeIn = new AlphaAnimation(0.0f , 1.0f ) ;
+        fadeIn.setDuration(2100);
+        fadeIn.setFillAfter(true);
+        logo.startAnimation(fadeIn);
+
+    }
+
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.logo);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
     }
 
     public class MyCountdownTimer extends CountDownTimer
