d7f3965636f8b0ece8e917be1985a9e032b9e5e3
Poolio <poolio.travel@gmail.com>
2016-07-30 21:58:09 +0530

    changes in splash screen

--- app/src/main/java/androarmy/poolio/SplashScreen.java
+++ app/src/main/java/androarmy/poolio/SplashScreen.java
@@ -1,6 +1,7 @@
 package androarmy.poolio;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -16,11 +17,21 @@ import android.widget.Toast;
 
 import com.onesignal.OneSignal;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class SplashScreen extends Activity {
@@ -30,7 +41,10 @@ public class SplashScreen extends Activity {
     public String NO_VAL="empty";
      //public final String SIGNIN_URL="http://192.168.1.6/poolio/signin.php";//Siddharth's pc
      public final String SIGNIN_URL="http://192.168.1.14:8080/poolio/signin.php";//Sumit's pc
+    public final String CONDITION_URL="http://192.168.1.14:8080/poolio/conditions.php";//Sumit's pc
     private String password = null;
+    int flag;
+    String heading, description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,26 +87,31 @@ public class SplashScreen extends Activity {
         @Override
         public void onTick(long millisUntilFinished)
         {
+            getCondition(CONDITION_URL);
 
         }
         @Override
 
-        public void onFinish()
-        {
+        public void onFinish() {
             myCountdownTimer.cancel();
-            SharedPreferences session = getSharedPreferences("session", MODE_PRIVATE);
-            String mob = session.getString("mobile", NO_VAL);
-            String pass = session.getString("password", NO_VAL);
+            if (flag == 0) {
 
-            if (mob.equalsIgnoreCase(NO_VAL) || pass.equalsIgnoreCase(NO_VAL) || mob.equalsIgnoreCase("") || pass.equalsIgnoreCase("")) {
-                Intent myIntent = new Intent(SplashScreen.this, WelcomeSlider.class);
-                SplashScreen.this.finish();
-                startActivity(myIntent);
-                overridePendingTransition(R.anim.next_slide_in, R.anim.next_slide_out);
+                SharedPreferences session = getSharedPreferences("session", MODE_PRIVATE);
+                String mob = session.getString("mobile", NO_VAL);
+                String pass = session.getString("password", NO_VAL);
+
+                if (mob.equalsIgnoreCase(NO_VAL) || pass.equalsIgnoreCase(NO_VAL) || mob.equalsIgnoreCase("") || pass.equalsIgnoreCase("")) {
+                    Intent myIntent = new Intent(SplashScreen.this, WelcomeSlider.class);
+                    SplashScreen.this.finish();
+                    startActivity(myIntent);
+                    overridePendingTransition(R.anim.next_slide_in, R.anim.next_slide_out);
+                } else {
+                    userLogin(mob, md5(pass));
+                }
             }
             else {
-                      userLogin(mob,md5(pass));
-             }
+
+            }
         }
         private void userLogin(final String mobile, final String password){
             class UserLoginClass extends AsyncTask<String,Void,String> {
@@ -134,6 +153,91 @@ public class SplashScreen extends Activity {
             ulc.execute(mobile,password);
         }
     }
+
+
+    public void getCondition(String url) {
+
+        class GetJSON extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                //loading = ProgressDialog.show(getContext(),"Please Wait",null,true,true);
+
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setReadTimeout(15000);
+                    con.setConnectTimeout(15000);
+                    con.setDoInput(true);
+                    con.setDoOutput(true);
+
+                    String response;
+
+                    int responseCode = con.getResponseCode();
+
+                    if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                        BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                        response = br.readLine();
+                        return response;
+                    } else {
+
+                        response = "Error";
+
+                        return response;
+
+                    }
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                    return "Exception";
+                }
+
+
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                //loading.dismiss();
+
+
+
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+                    JSONArray result = jsonObject.getJSONArray("result");
+                    JSONObject c = result.getJSONObject(0);
+                    flag = c.getInt("id");
+                    if(flag!=0)
+                    {
+                        heading=c.getString("heading");
+                        description=c.getString("description");
+                    }
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+        GetJSON gj = new GetJSON();
+        gj.execute(url);
+    }
+
 
     public String md5(String s){
         MessageDigest digest ;
